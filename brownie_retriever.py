cc2ab5695b67a0826b001adf664679d4537a3227
Beier <Beierlejc@gmail.com>
2022-12-31 18:46:07 -0600

    Selenium bot

---
+++ brownie_retriever.py
@@ -0,0 +1,106 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+async def get_brownie(store_number: str, date: str, time: str, order_id: str, receipt_location: str) -> int:
+    if len(str(store_number)) != 4:
+        return -1
+    if len(str(order_id)) != 10:
+        return -2
+
+    url = "https://tellslimchickens.smg.com/"
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+    # Input details
+    store_number_input = driver.find_element(By.NAME, value="InputStoreNum")
+    store_number_input.send_keys(store_number)
+
+    driver.find_element(By.CLASS_NAME, value="ui-datepicker-trigger").click()  # Open calender widget
+    driver.find_element(By.LINK_TEXT, value=str(date.split("/")[1])).click()
+
+    split_time = time.split(":")
+    hour = split_time[0]
+    minute = split_time[1].split(" ")[0]
+    meridian = split_time[1].split(" ")[1].upper()
+    if len(hour) == 1:
+        hour = "0" + hour
+    if len(minute) == 1:
+        minute = "0" + minute
+    hour_input = Select(driver.find_element(By.NAME, value="InputHour"))
+    minute_input = Select(driver.find_element(By.NAME, value="InputMinute"))
+    meridian_input = Select(driver.find_element(By.NAME, value="InputMeridian"))
+    hour_input.select_by_value(hour)
+    minute_input.select_by_value(minute)
+    meridian_input.select_by_value(meridian)
+    order_id_input = driver.find_element(By.NAME, value="InputCheckNum")
+    order_id_input.send_keys(order_id)
+    # Submit
+    driver.find_element(By.NAME, value="NextButton").click()
+
+    highly_satisfied_button = ""
+    while highly_satisfied_button == "":
+        highly_satisfied_button = driver.find_element(By.CLASS_NAME, value="radioSimpleInput")
+    highly_satisfied_button.click()
+    driver.find_element(By.ID, value="NextButton").click()
+
+    driver.find_element(By.XPATH, value=f"//label[contains(text(), '{receipt_location}')]/preceding-sibling::span[1]").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    buttons = driver.find_elements(By.XPATH, value='//td[@aria-describedby="HighlySatisfiedNeitherDESC5"]')
+    for button in buttons:
+        button.find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+    # Submit
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    buttons = driver.find_elements(By.XPATH, value='//td[@aria-describedby="HighlySatisfiedNeitherDESC5"]')
+    for button in buttons:
+        button.find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+        # Submit
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    # No problems had
+    driver.find_element(By.XPATH, value='//td[@aria-describedby="YesNoASC2"]').find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    # Likelihood
+    elements = driver.find_elements(By.XPATH, value='//td[@aria-describedby="HighlyLikelyDESC5"]')
+    for element in elements:
+        element.find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    da_paragraph = driver.find_element(By.NAME, value="S000024")
+    da_paragraph.send_keys("I had an excellent time at Slim Chicken's. The food was wonderful, and it was just the right temperature. When I ordered the staff were very polite and made the experience all the more easy.")
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    elements = driver.find_elements(By.XPATH, value='//td[@aria-describedby="YesNoASC1"]')
+    for element in elements:
+        element.find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    driver.find_element(By.XPATH, value='//td[@aria-describedby="YesNoASC1"]').find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    driver.find_element(By.XPATH, value='//td[@aria-describedby="YesNoASC2"]').find_element(By.CLASS_NAME,value="radioSimpleInput").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    driver.find_element(By.XPATH, value=f"//label[contains(text(), 'Four or more')]/preceding-sibling::span[1]").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    driver.find_element(By.XPATH, value=f"//label[contains(text(), 'Convenience of location')]/preceding-sibling::span[1]").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    age = Select(driver.find_element(By.NAME, value="R000035"))
+    age.select_by_value("2")
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    driver.find_element(By.XPATH, value='//td[@aria-describedby="YesNoASC2"]').find_element(By.CLASS_NAME, value="radioSimpleInput").click()
+    driver.find_element(By.CLASS_NAME, value="NextButton").click()
+
+    code = driver.find_element(By.CLASS_NAME, value="ValCode").text.split(": ")[1]
+    return int(code)
+
+
+if __name__ == "__main__":
+    print("Code:", get_brownie("0000", "10/12/1997", "1:54 PM", "0000000000", "Drive thru"))
--- discord_bot.py
+++ discord_bot.py
@@ -2,6 +2,9 @@ import discord
 import os
 from dotenv import load_dotenv
 
+from brownie_retriever import get_brownie
+
+
 load_dotenv()  # load all the variables from the env file
 bot = discord.Bot()
 
@@ -12,8 +15,12 @@ async def on_ready():
 
 
 @bot.slash_command(name="get_brownie", description="Fill out online servey for free brownies!")
-async def get_brownie(ctx, order_id: discord.Option(int), teller_number: discord.Option(int)):
-    await ctx.respond("Processing...")
+async def get_brownie(ctx, store_number: discord.Option(str), date: discord.Option(str), time: discord.Option(str), order_id: discord.Option(str), receipt_location: discord.Option(str)):
+    await ctx.respond("Code:" + str(get_brownie(store_number=store_number, date=date, time=time, order_id=order_id, receipt_location=receipt_location)))
 
+
+@bot.slash_command(name="help", description="This is the help command...bruh")
+async def help(ctx):
+    await ctx.respond("Use /get_brownie <store number> <date (MM/DD/YY)> <time (HH:MM AM/PM)> <order id> <receipt location>\nWhen using receipt location: enter one of the following: Drive thru, Delivery, Dine in, Curbside Pickup, In-store pickup")
 
 bot.run(os.getenv('TOKEN'))  # run the bot with the token
